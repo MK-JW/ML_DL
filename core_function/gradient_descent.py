7f6b2be5ba19ad41c8289784cd84f5679940765a
MK-JW <minjinwuwel1@163.com>
2025-02-27 16:41:42 +0800

    线性回归函数封装

--- core_function/gradient_descent.py
+++ core_function/gradient_descent.py
@@ -1,13 +1,14 @@
 __author__ = 'Minjinwu'
 
-import sys
-print(sys.path)
+# import sys
+# print(sys.path)
 import numpy as np
 from data_process.data_normolized import Data_normolized
 
 
 #执行梯度下降
 class Gradient_descent: 
+
     def __init__(self, x_train, y_train):
         (
 
@@ -19,57 +20,50 @@ class Gradient_descent:
         self.x_train = x_train
         self.y_train = y_train
         
-
-        self.sample_num, self.features_num = np.shape(self.x_train)
-
-
-        def grad_descent(self, alpha, x_current):
+        self.sample_num, self.features_num = self.x_train.shape
 
 
-            loss = np.array([])
-            k = 0
-            w_k = x_current[0:self.features_num,:]
-            b_k = x_current[-1]
-            y_prediction = np.dot(self.x_train, w_k) + b_k
-            loss = np.append(loss, self.cost_function(y_prediction))
-            dw_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, x_train)
-            db_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, \
-                            np.ones(self.sample_num, self.features_num))
-            x_current = np.vstack((w_k, b_k))
-            d_current = np.vstack((dw_k, db_k))
+    def get_cost(self, y_prediciton):
 
-            k = 1
-            x_next = x_current + alpha*d_current
-            y_prediction = np.dot(self.x_train, w_k) + b_k
-            loss = np.append(loss, self.cost_function(y_prediction))
-            dw_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, x_train)
-            db_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, \
-                            np.ones(self.sample_num, self.features_num))
-            d_current = np.vstack((dw_k, db_k))
-            while np.linalg.norm(x_next - x_current)> self.tol:
-                 k += 1
-                 x_current = x_next
-                 x_next = x_current + alpha*d_current
-                 y_prediction = np.dot(self.x_train, w_k) + b_k
-                 loss = np.append(loss, self.cost_function(y_prediction))
-                 dw_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, x_train)
-                 db_k = -(1/self.sample_num)*np.dot((y_prediction - y_train).T, \
-                            np.ones(self.sample_num, self.features_num))
-                 d_current = np.vstack((dw_k, db_k))
+        cost = (1/(2*self.sample_num))*np.sum(((y_prediciton - self.y_train)**2))
 
-            
-            return w_k, b_k, loss, k
+        return cost
+    
+
+    def get_prediction(self, w, b):
+
+        y_prediction = np.dot(self.x_train, w) + b
+
+        return y_prediction
+    
+
+    def get_gradient(self, y_prediction):
+        
+        dw = (1/self.sample_num)*np.dot((y_prediction - self.y_train).T, self.x_train).T
+        db = (1/self.sample_num)*np.dot((y_prediction - self.y_train).T, np.ones((self.sample_num, 1)))
+        d_current = np.vstack((dw, db))
+
+        return d_current
+    
+
+    def get_loss(self, cost):
+
+        loss = np.array([])
+        
+        loss = np.append(loss, cost)
+
+        return loss
+
+
+    def upgrade_parameters(self, alpha, x_current, d_current):
+
+        x_next = x_current + alpha*d_current
+
+        return x_next
+    
+
+
+
+
         
 
-        def predict(self, alpha, x_current):
-
-            w,b = self.gradient_descent(alpha, x_current)
-            predicition = x_train*w + b
-            return predicition
-        
-
-        def  cost_function(self, y_prediction):
-
-            cost = (1/(2*self.sample_num))*np.sum((y_prediction - self.y_train)**2)
-
-            return cost
