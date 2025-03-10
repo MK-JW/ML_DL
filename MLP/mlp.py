aeefbfcfa03bc15d2c7b0a6e14c8d6a247cdda6c
MK-JW <minjinwuwel1@163.com>
2025-03-10 21:39:49 +0800

    多层感知机实现

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -1,13 +1,13 @@
 __author__ = 'minjinwu'
 
 
-import sys
-print(sys.path)
+# import sys
+# print(sys.path)
 import os 
 import torch
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from MLP.linear_layer import Linear_layer
 from data_process.data_normolized import Data_normalized
 
@@ -18,23 +18,29 @@ data_path = os.path.join(current_dir,'..','world-happiness-report-2017.csv')
 data = pd.read_csv(data_path)
 x_train = data[['Economy..GDP.per.Capita.']].values
 y_train = data[['Happiness.Score']].values
+# print(x_train.shape, y_train.shape)
 
 
 # 超参数设置
-alpha = 0.001
+tol = 10**-6
+alpha = 0.01
 epoch = 5
 batch_size = 31
 neurons_num = 3
 
 
 # 训练神经网络
-
+loss_history = []
 sample_num, features_num = x_train.shape
-h_current = np.random.uniform(low=0, high=0.2, size = (features_num + 1, neurons_num))
-o_current = np.random.uniform(low=0.05, high=0.15, size = (neurons_num + 1, 1))
+h_current = np.random.uniform(low=0, high=0.005, size = (features_num + 1, neurons_num))
+o_current = np.random.uniform(low=0.01, high=0.02, size = (neurons_num + 1, 1))
+# print(h_current)
+# print(o_current)
+
 
 for i in range(epoch):
 
+    loss = np.array([])
     index = np.random.permutation(sample_num)
     x_shuffled = x_train[index]
     y_shuffled = y_train[index]
@@ -60,7 +66,24 @@ for i in range(epoch):
         x_tr = x_shuffled[start:end, :]
         y_tr = y_shuffled[start:end, :]
 
-        prediction = Linear_layer(x_tr, y_tr).forward_propagation(h_current, o_current, neurons_num)
-        h_current, o_current = Linear_layer(x_tr, y_tr).back_propagation(h_current, o_current, neurons_num, alpha)
+        prediction, loss = Linear_layer(x_tr, y_tr).forward_propagation(h_current, o_current, neurons_num, loss)
+        h_next, o_next = Linear_layer(x_tr, y_tr).back_propagation(h_current, o_current, neurons_num, alpha)
 
+        while np.linalg.norm(h_next - h_current)>tol and np.linalg.norm(o_next - o_current)>tol:
+
+            h_current, o_current = h_next, o_next
+            prediction, loss = Linear_layer(x_tr, y_tr).forward_propagation(h_current, o_current, neurons_num, loss)
+            h_next, o_next = Linear_layer(x_tr, y_tr).back_propagation(h_current, o_current, neurons_num, alpha)
         
+    # loss_history = np.append(loss_history, loss)
+    loss_history.append(loss)
+
+print(loss_history)
+# 结果的可视化
+# print(x_train.shape)
+# x_train = Data_normalized(x_train)
+# plt.figure(1)
+# plt.scatter(x_train, y_train, color = 'b', marker = 'o')
+# plt.figure(2)
+# plt.plot(np.arange(len(loss)), loss, color = 'b')
+# plt.show()
